3c88be1c46fd6822f49f76c1fba83c9a0bae2ac3
Dan Gilleland <dgilleland@nait.ca>
2014-10-07 09:30:31 -0600

    Added NOT NULL and PK/FK constraint names

--- ESP/ESP - DDL.sql
+++ ESP/ESP - DDL.sql
@@ -21,41 +21,45 @@ GO
 CREATE TABLE Customers
 (
     CustomerNumber  int
+		CONSTRAINT PK_Customers_CustomerNumber
         PRIMARY KEY
-        IDENTITY(100, 1)            ,
-    FirstName       varchar(50)     ,
-    LastName        varchar(60)     ,
-    [Address]       varchar(40)     ,
-    City            varchar(35)     ,
-    Province        char(2)         ,
-    PostalCode      char(6)         ,
-    PhoneNumber     char(13)
+        IDENTITY(100, 1)            NOT NULL, -- NOT NULL when data is Required
+    FirstName       varchar(50)     NOT NULL,
+    LastName        varchar(60)     NOT NULL,
+    [Address]       varchar(40)     NOT NULL,
+    City            varchar(35)     NOT NULL,
+    Province        char(2)         NOT NULL,
+    PostalCode      char(6)         NOT NULL,
+    PhoneNumber     char(13)		    NULL  -- Optional - can be "blank"
 )
 GO
 
 CREATE TABLE CustomerOrders
 (
     OrderNumber     int
-        PRIMARY KEY
-        IDENTITY(200,1)                 ,
+        CONSTRAINT PK_CustomerOrders_OrderNumber
+			PRIMARY KEY
+        IDENTITY(200,1)                 NOT NULL,
     CustomerNumber  int
-        FOREIGN KEY REFERENCES
-            Customers(CustomerNumber)   ,
-    [Date]          datetime            ,
-    Subtotal        money               ,
-    GST             money               ,
-    Total           money           
+        CONSTRAINT FK_CustomerOrders_CustomerNumber_Customers_CustomerNumber
+		FOREIGN KEY REFERENCES
+            Customers(CustomerNumber)   NOT NULL,
+    [Date]          datetime            NOT NULL,
+    Subtotal        money               NOT NULL,
+    GST             money               NOT NULL,
+    Total           money				NOT NULL
 )
 GO
 
 CREATE TABLE InventoryItems
 (
     ItemNumber          varchar(5)
-        PRIMARY KEY                     ,
-    ItemDescription     varchar(50)     ,
-    CurrentSalePrice    money           ,
-    InStockCount        int             ,
-    ReorderLevel        int
+        CONSTRAINT PK_InventoryItems_ItemNumber
+		PRIMARY KEY                     NOT NULL,
+    ItemDescription     varchar(50)     NOT NULL,
+    CurrentSalePrice    money           NOT NULL,
+    InStockCount        int             NOT NULL,
+    ReorderLevel        int				NOT NULL
 )
 GO
 
@@ -64,13 +68,24 @@ CREATE TABLE OrderDetails
 (
     OrderNumber         int
         FOREIGN KEY REFERENCES
-            CustomerOrders(OrderNumber) ,
+            CustomerOrders(OrderNumber) NOT NULL,
     ItemNumber          varchar(5)
         FOREIGN KEY REFERENCES
-            InventoryItems(ItemNumber)  ,
-    Quantity            smallint        ,
-    SellingPrice        money           ,
-    Amount              money           ,
+            InventoryItems(ItemNumber)  NOT NULL,
+    Quantity            smallint        NOT NULL,
+    SellingPrice        money           NOT NULL,
+    Amount              money           NOT NULL,
     -- The following is a Table Constraint
     PRIMARY KEY (OrderNumber, ItemNumber)
 )
+
+
+
+
+
+
+
+
+
+
+
